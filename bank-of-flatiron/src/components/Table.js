6b93291243494746312c6ddef557e1393711b2be
Stephen Muriithi Munene <smuriithi7969@gmail.com>
2024-04-27 22:29:31 +0300

    submitted final static components

--- bank-of-flatiron/src/components/Table.js
+++ bank-of-flatiron/src/components/Table.js
@@ -4,20 +4,20 @@ function Table(){
  return(
     <>
     <table>
-     <tbody>   
+        
     <tr className="row">
      <th className="date">Date</th>
     <th className="description">Description</th>
     <th className="category">Category</th>    
     <th className="amount">Amount</th>
     </tr>
+    <tbody>
     <tr> 
     <td>2024</td>
-         </tr>
-    <tr>
-        <td>Music </td></tr>
-    <tr> <td>News</td></tr>
-    <tr> <td>schyeah!</td></tr>
+    <td>Music </td>
+     <td>News</td>
+     <td>schyeah!</td>
+     </tr>
     </tbody>
     </table>
     </>
